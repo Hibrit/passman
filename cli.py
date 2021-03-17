c85ab517c83f241081a1c327837b839e53e7d339
Hibrit <taskin.n.dogan@yandex.com>
2021-03-17 17:24:21 +0300

    some progress

--- cli.py
+++ cli.py
@@ -2,51 +2,50 @@
 
 from curses import initscr
 from sys import exit
+from password_generator import PasswdGenerator
+from pyperclip import copy
+from time import sleep
 
 
 class CLI:
     def __init__(self):
         self.screen = initscr()
         self.current_options = [i for i in 'ludp']
+        self.old_options = None
         self.length = 16
+        self.old_length = None
         self.description = None
-        self.current_password = None
+        self.login_info = None
+        self.password_generator = None
+        self.password = None
 
-    def set_password_options(self):
-        # TODO construct a menu that will control password options
-        pass
-
-    def set_description(self):
-        # TODO construct a menu that will set password description for the current password
-        pass
-
-    def save_current_password(self):
-        # TODO construct a function that will save the current password to a sqlite database
-        pass
-
-    def copy_current_password(self):
-        # TODO construct a function that will copy the current password to the clipboard
-        pass
-
-    def generate_password_menu(self):
+    # * Done
+    def set_length(self):
         self.screen.clear()
-        # TODO if there is a description make it appear in the message
+        self.screen.addstr(0, 0, 'please specify a password length >> ')
+        self.screen.refresh()
+        self.length = int(self.screen.getstr(0, 36, 10))
+        self.set_password_options()
+
+    # * Done
+    def set_password_options(self):
+        if self.old_options is None:
+            self.old_options = self.current_options.copy()
+        if self.old_length is None:
+            self.old_length = self.length
+
+        self.screen.clear()
         message = [
-            '============ Password Generation ============',
+            '============ Set Options ============',
             f'current options are >> {"".join(self.current_options)}',
             f'password length >> {self.length}',
-            # TODO all of this options will be inside of set password generation options
-            # '(l) to toggle option lowercase',
-            # '(u) to toggle option uppercase',
-            # '(d) to toggle option digits',
-            # '(p) to toggle option puctuation',
-            # '(x) to set password length',
-            '(o) set password generating options',
-            '(d) add description',
-            '(g) generate or regenerate password',
-            '(s) save current password',
-            '(c) copy current password',
-            '(m) main menu',
+            '(l) to toggle option lowercase',
+            '(u) to toggle option uppercase',
+            '(d) to toggle option digits',
+            '(p) to toggle option puctuation',
+            '(x) to set password length',
+            '(s) save current settings and return to generation',
+            '(g) discard current settings and return to generation',
             '(q) quit',
         ]
 
@@ -60,13 +59,165 @@ class CLI:
         if usr_inp == 'q':
             quit()
         elif usr_inp == 'g':
-            # TODO generate or regenerate password
-            #! call itself recursively with an extra option that will make this fuction generate and show password
+            self.current_options = self.old_options.copy()
+            self.old_options = None
+            self.length = self.old_length
+            self.old_length = None
+            self.generate_password_menu()
+        elif usr_inp == 's':
+            self.old_options = None
+            self.old_length = None
+            self.generate_password_menu()
+        elif usr_inp == 'x':
+            self.set_length()
+        elif usr_inp == 'l':
+            if 'l' not in self.current_options:
+                self.current_options.append('l')
+            else:
+                self.current_options.remove('l')
+        elif usr_inp == 'u':
+            if 'u' not in self.current_options:
+                self.current_options.append('u')
+            else:
+                self.current_options.remove('u')
+        elif usr_inp == 'd':
+            if 'd' not in self.current_options:
+                self.current_options.append('d')
+            else:
+                self.current_options.remove('d')
+        elif usr_inp == 'p':
+            if 'p' not in self.current_options:
+                self.current_options.append('p')
+            else:
+                self.current_options.remove('p')
+        else:
+            self.error_scr('there is no such option',
+                           self.set_password_options)
+
+        self.set_password_options()
+
+    #! In progress
+    def set_info(self):
+        self.screen.clear()
+        message = [
+            '============ Set Information ============',
+            # TODO show info here if possible
+            '(n) set name',
+            '(d) set description',
+            '(s) save information and return to generation',
+            '(g) discard information and return to generation',
+            '(q) quit',
+        ]
+
+        for index, line in enumerate(message):
+            self.screen.addstr(index, 0, line)
+
+        self.screen.refresh()
+
+        usr_inp = self.screen.getkey().lower()
+
+        if usr_inp == 'q':
+            quit()
+        elif usr_inp == 'n':
+            #! set name
             pass
+        elif usr_inp == 'd':
+            #! set description
+            pass
+        elif usr_inp == 's':
+            #! save info and return generation
+            pass
+        elif usr_inp == 'g':
+            #! discard info and return generation
+            pass
+
+    #! In progress
+    def save_current_password(self):
+        # TODO construct a function that will save the current password to a sqlite database
+        pass
+
+    # * Done
+    def copy_current_password(self):
+        copy(self.password)
+        self.screen.clear()
+        message = [
+            'Password copied'
+        ]
+        for index, line in enumerate(message):
+            self.screen.addstr(index, 0, line)
+
+        self.screen.refresh()
+
+        sleep(1)
+
+        self.generate_password_menu()
+
+    #! In progress
+    def generate_password_menu(self, gen=False):
+        self.screen.clear()
+        if gen:
+            self.password_generator = PasswdGenerator(
+                options=''.join(self.current_options), length=self.length)
+            self.password = self.password_generator.generate()
+            message = [
+                '============ Password Generation ============',
+                f'current options are >> {"".join(self.current_options)}',
+                f'password length >> {self.length}',
+                # TODO if there is a description make it appear in the message
+                f'current password >> {self.password}',
+                '(o) set password generating options',
+                '(d) add description',
+                '(g) generate or regenerate password',
+                '(s) save current password',
+                '(c) copy current password',
+                '(m) main menu',
+                '(q) quit',
+            ]
+        elif self.password:
+            message = [
+                '============ Password Generation ============',
+                f'current options are >> {"".join(self.current_options)}',
+                f'password length >> {self.length}',
+                # TODO if there is a description make it appear in the message
+                f'current password >> {self.password}',
+                '(o) set password generating options',
+                '(d) add description',
+                '(g) generate or regenerate password',
+                '(s) save current password',
+                '(c) copy current password',
+                '(m) main menu',
+                '(q) quit',
+            ]
+        else:
+            message = [
+                '============ Password Generation ============',
+                f'current options are >> {"".join(self.current_options)}',
+                f'password length >> {self.length}',
+                # TODO if there is a description make it appear in the message
+                '(o) set password generating options',
+                '(d) add description',
+                '(g) generate or regenerate password',
+                '(s) save current password',
+                '(c) copy current password',
+                '(m) main menu',
+                '(q) quit',
+            ]
+
+        for index, line in enumerate(message):
+            self.screen.addstr(index, 0, line)
+
+        self.screen.refresh()
+
+        usr_inp = self.screen.getkey().lower()
+
+        if usr_inp == 'q':
+            quit()
+        elif usr_inp == 'g':
+            self.generate_password_menu(gen=True)
         elif usr_inp == 'o':
             self.set_password_options()
         elif usr_inp == 'd':
-            self.set_description()
+            self.set_info()
         elif usr_inp == 's':
             self.save_current_password()
         elif usr_inp == 'c':
@@ -74,12 +225,15 @@ class CLI:
         elif usr_inp == 'm':
             self.main_menu()
         else:
-            self.error_scr('there is no such option')
+            self.error_scr('there is no such option',
+                           self.generate_password_menu)
 
-    def error_scr(self, message):
-        # TODO construct an error screen
+    #! In progress
+    def error_scr(self, message, function):
+        # TODO construct an error screen which will be continued after if possible
         pass
 
+    #! In progress
     def main_menu(self):
         self.screen.clear()
         message = [
@@ -105,7 +259,7 @@ class CLI:
         elif usr_inp == 'q':
             exit()
         else:
-            self.error_scr('there is no such option')
+            self.error_scr('there is no such option', self.main_menu)
 
 
 if __name__ == '__main__':
